541ab40ee6ddfd620b3e3afc5e3962fc056847a2
zzz-creator <84800058+zzz-creator@users.noreply.github.com>
2026-05-24 13:50:47 -0400

    Update vehicles.js
    Signed-off-by: zzz-creator <84800058+zzz-creator@users.noreply.github.com>

--- vehicles.js
+++ vehicles.js
@@ -1454,7 +1454,7 @@ cg.createImage({file:"vehicle_grey.png"},"vehicle_grey");
 const vehicles = {};
 
 (()=>{
-  const vehicleStartPriority = [1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,31,32,33,34,35,36,37,38];
+  const vehicleStartPriority = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28.29.30,31,32,33,34,35,36,37,38];
 
   const positions = {};
   for (const posName in disco.discoPositions) {
